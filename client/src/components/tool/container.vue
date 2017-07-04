e368a96f6f36d45f3118549301378eff686caba7
451354@qq.com <451354@qq.com>
2017-07-04 15:44:58 +0800

    add：上传文件

--- client/src/components/tool/container.vue
+++ client/src/components/tool/container.vue
@@ -8,6 +8,13 @@
         </el-menu-item-group>
       </el-menu>
     </div>
+    <div class="right">
+      <form id="form-demo" method="POST" enctype="multipart/form-data" :action="menuApi" ref="uploadform">
+        <label for="file">选择上传的文件</label>
+        <input type="file" id="file" name="file">
+        <button :click="postFile">确定</button>
+      </form>
+    </div>
   </div>
 </template>
 <script>
@@ -15,14 +22,23 @@ import Util from '../../assets/js/Util.js'
 export default {
   data: function () {
     return {
-      menuApi: Util.GetApiRootPath() + 'tool/toolmenu',
+      menuApi: Util.GetApiRootPath() + 'tool',
       toolMenu: null
     }
   },
-  created: function () {
-    this.$http.get(this.menuApi).then(response => {
-      this.toolMenu = response.data
-    })
+  mounted: function () {
+
+  },
+  methods: {
+    postFile: function () {
+      let file = this.$refs.uploadform.file
+      debugger
+      let formData = new FormData()
+      formData.append('file', file)
+      this.$http.post(this.menuApi, formData).then(response => {
+        this.toolMenu = response.data
+      })
+    }
   }
 }
 </script>
